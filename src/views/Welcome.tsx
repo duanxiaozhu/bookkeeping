432d9dd9e59bf17361278c6fc8d4066697d0c665
zhuolin <zhuolin1014@163.com>
2023-06-11 11:57:53 +0800

    滑动切换路由

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,31 @@
-import { defineComponent, Transition, VNode } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router';
 import s from './welcome.module.scss'
-import logo from '../assets/icons/flower.svg'
+import { useSwipe } from '../hooks/useSwipe';
+import { throttle } from '../shared/throttle';
+
+const pushMap: Record<string, string> = {
+    'Welcome1': '/welcome/2',
+    'Welcome2': '/welcome/3',
+    'Welcome3': '/welcome/4',
+    'Welcome4': '/start',
+}
+
 export const Welcome = defineComponent({
     setup: (props, context) => {
+        const main = ref<HTMLElement>()
+        const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+        const route = useRoute()
+        const router = useRouter()
+        const push = throttle(() => {
+            const name = (route.name || 'Welcome1').toString()
+            router.push(pushMap[name])
+        }, 500)
+        watchEffect(() => {
+            if (swiping.value && direction.value === 'left') {
+                push()
+            }
+        })
         return () => (
             <div class={s.wrapper}>
                 <header>
@@ -12,7 +34,7 @@ export const Welcome = defineComponent({
                         <use xlinkHref='#flower'></use>
                     </svg>
                 </header>
-                <main class={s.main}>
+                <main class={s.main} ref={main}>
                     <RouterView name="main">
                         {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
                             <Transition
