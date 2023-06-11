7b8dfbff915f5d67b1256f7e8710015d64377945
zhuolin <zhuolin1014@163.com>
2023-06-11 15:08:03 +0800

    完成FloatButton组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,19 @@
+import { PropType, defineComponent } from 'vue';
+import s from './Icon.module.scss';
+export type IconName = 'add'| 'chart'|'click'|'cloud'|'pig'|'skip'|'next'|'flower'
+
+export const Icon = defineComponent({
+    props:{
+        name:{
+            type:String as PropType<IconName>,
+            required:true,
+        }
+    },
+    setup: (props, context) => {
+        return () => (
+            <svg class={s.icon}>
+                <use xlinkHref={'#'+props.name}></use>
+            </svg>
+        )
+    }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,16 +1,18 @@
 import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
 import s from './StartPage.module.scss';
+import { FloatButton } from '../shared/FloatButton';
 export const StartPage = defineComponent({
     setup: (props, context) => {
         const onClick=()=>{
-            
+
         }
         return () => (
             <div>
                 <div class={s.button_wrapper}>
                 <Button class={s.button} onClick={onClick}>测试</Button>
                 </div>
+                <FloatButton iconName='add'/>
             </div>
         )
     }
