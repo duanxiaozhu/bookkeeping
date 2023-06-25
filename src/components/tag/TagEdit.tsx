de43dab10035375e4985e3a15b0ff376a561fd6d
zhuolin <zhuolin1014@163.com>
2023-06-26 00:30:17 +0800

    长按编辑

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,37 +1,23 @@
 import s from './Tag.module.scss'
-import { defineComponent, reactive } from 'vue';
+import { defineComponent } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
-import { Rules, validate } from '../../shared/validate';
-import { Icon } from '../../shared/Icon';
 import { Button } from '../../shared/Button';
 import { TagForm } from './TagForm';
 import { BackIcon } from '../../shared/BackIcon';
+import { useRoute } from 'vue-router';
 export const TagEdit = defineComponent({
     setup: (props, context) => {
-        const formData=reactive({
-            name:'',
-            sign:'',
-        })
-        const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
-        const onSubmit = (e: Event) => {
-          const rules: Rules<typeof formData> = [
-            { key: 'name', type: 'required', message: '必填' },
-            { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
-            { key: 'sign', type: 'required', message: '选择一个表情' },
-          ]
-          Object.assign(errors, {
-            name: undefined,
-            sign: undefined
-          })
-          Object.assign(errors, validate(formData, rules))
-          e.preventDefault()
-        }
+      const route = useRoute()
+      const numberId = parseInt(route.params.id!.toString())
+      if(Number.isNaN(numberId)){
+        return ()=> <div>id 不存在</div>
+      }
         return () => (
             <MainLayout>{{
                 title:()=>'编辑标签',
                 icon: () => <BackIcon/>,
                 default:()=><>
-                    <TagForm/>
+                    <TagForm id={numberId}/>
                     <div class={s.actions}>
                     <Button level='danger' class={s.removeTags} onClick={()=>{}}>删除标签</Button>
                     <Button level='danger' class={s.removeTagsAndItems} onClick={()=>{}}>删除标签和记账</Button>
--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,26 +1,26 @@
-import { defineComponent, PropType, reactive } from "vue";
+import { defineComponent, onMounted, PropType, reactive } from "vue";
 import s from "./Tag.module.scss";
 import { Form, FormItem } from "../../shared/Form";
 import { Button } from "../../shared/Button";
 import { hasError, Rules, validate } from "../../shared/validate";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { http } from "../../shared/Http";
 import { onFormError } from "../../shared/onFormError";
 export const TagForm = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>,
-    },
+    id: Number,
   },
   setup: (props, context) => {
     const route = useRoute();
-    const formData = reactive({
+    const formData = reactive<Partial<Tag>>({
+      id: undefined,
       name: "",
       sign: "",
       kind: route.query.kind!.toString(),
     });
     const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
-    const onSubmit =async (e: Event) => {
+    const router = useRouter();
+    const onSubmit = async (e: Event) => {
       e.preventDefault();
       const rules: Rules<typeof formData> = [
         { key: "name", type: "required", message: "必填" },
@@ -37,11 +37,29 @@ export const TagForm = defineComponent({
         sign: [],
       });
       Object.assign(errors, validate(formData, rules));
-      if(!hasError(errors)){
-        const response=await http.post('/tags',formData,{params:{_mock:'tagCreate'},}).catch((error)=>
-        onFormError(error, (data)=> Object.assign(errors, data.errors)))
+      if (!hasError(errors)) {
+        const promise = (await formData.id)
+          ? http.patch(`/tags/${formData.id}`, formData, {
+              params: { _mock: "tagEdit" },
+            })
+          : http.post("/tags", formData, {
+              params: { _mock: "tagCreate" },
+            });
+        await promise.catch((error) =>
+          onFormError(error, (data) => Object.assign(errors, data.errors))
+        );
+        router.back();
       }
     };
+    onMounted(async () => {
+      if (!props.id) {
+        return;
+      }
+      const response = await http.get<Resource<Tag>>(`/tags/${props.id}`, {
+        _mock: "tagShow",
+      });
+      Object.assign(formData, response.data.resource);
+    });
     return () => (
       <Form onSubmit={onSubmit}>
         <FormItem
