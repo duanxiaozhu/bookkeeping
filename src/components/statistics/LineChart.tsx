2d0d90afb10e2ef2296446b625a5d85ac0c1d70f
zhuolin <zhuolin1014@163.com>
2023-06-27 13:39:09 +0800

    美化折线图

--- src/components/statistics/LineChart.tsx
+++ src/components/statistics/LineChart.tsx
@@ -1,54 +1,137 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
-import s from './LineChart.module.scss';
-import * as echarts from 'echarts';
+import { defineComponent, onMounted, PropType, ref } from "vue";
+import s from "./LineChart.module.scss";
+import * as echarts from "echarts";
+import { Time } from "../../shared/time";
+import { getMoney } from "../../shared/Money";
+
+const option = {
+  tooltip: {
+    show: true,
+    trigger: "axis",
+    formatter: ([item]: any) => {
+      const [x, y] = item.data;
+      return `${new Time(new Date(x)).format(
+        "YYYY年MM月DD日"
+      )} ￥${getMoney(y)}`;
+    },
+  },
+  grid: [{ left: 10, top: 0, right: 10, bottom: 30 }],
+  xAxis: {
+    type: "category",
+    axisLabel: {
+      formatter: (value: string) =>
+        new Time(new Date(value)).format("MM-DD"),
+    },
+    boundaryGap: ["1%", "1%"], // 左右边界间隙
+    axisTick: {
+      alignWithLabel: true,
+    },
+    axisPointer: {
+      snap: true,
+    },
+  },
+  yAxis: {
+    type: "value",
+    show: true,
+    splitLine: {
+      show: true,
+      lineStyle: {
+        type: "dashed",
+      },
+    },
+    axisLabel: {
+      show: false,
+    },
+  },
+
+};
 export const LineChart = defineComponent({
   setup: (props, context) => {
-    const refDiv = ref<HTMLDivElement>()
-    onMounted(()=>{
-    if(refDiv.value===undefined){return}
-    var myChart = echarts.init(refDiv.value);
-    const option = {
-      grid: [
-        { left: 10, top: 0, right: 10, bottom: 30 }
-      ],
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        boundaryGap: ["1%", "1%"], // 左右边界间隙
-        axisTick: {
-          alignWithLabel: true,
-        },
-        axisPointer: {
-          snap: true,
-        },
-      },
-      yAxis: {
-        type: 'value',
-        show:true,
-        splitLine:{
-          show:true,
-          lineStyle:{
-            type:'dashed'
+    const refDiv = ref<HTMLDivElement>();
+    const data = [
+      ["2018-01-01T00:00:00.000+0800", 150],
+      ["2018-01-02T00:00:00.000+0800", 230],
+      ["2018-01-03T00:00:00.000+0800", 224],
+      ["2018-01-04T00:00:00.000+0800", 218],
+      ["2018-01-05T00:00:00.000+0800", 135],
+      ["2018-01-06T00:00:00.000+0800", 147],
+      ["2018-01-07T00:00:00.000+0800", 260],
+      ["2018-01-08T00:00:00.000+0800", 300],
+      ["2018-01-09T00:00:00.000+0800", 200],
+      ["2018-01-10T00:00:00.000+0800", 300],
+      ["2018-01-11T00:00:00.000+0800", 400],
+      ["2018-01-12T00:00:00.000+0800", 500],
+      ["2018-01-13T00:00:00.000+0800", 400],
+      ["2018-01-14T00:00:00.000+0800", 300],
+      ["2018-01-15T00:00:00.000+0800", 200],
+      ["2018-01-16T00:00:00.000+0800", 100],
+      ["2018-01-17T00:00:00.000+0800", 200],
+      ["2018-01-18T00:00:00.000+0800", 300],
+      ["2018-01-19T00:00:00.000+0800", 400],
+      ["2018-01-20T00:00:00.000+0800", 500],
+      ["2018-01-21T00:00:00.000+0800", 600],
+      ["2018-01-22T00:00:00.000+0800", 700],
+      ["2018-01-23T00:00:00.000+0800", 800],
+      ["2018-01-24T00:00:00.000+0800", 900],
+      ["2018-01-25T00:00:00.000+0800", 1000],
+      ["2018-01-26T00:00:00.000+0800", 1100],
+      ["2018-01-27T00:00:00.000+0800", 1200],
+      ["2018-01-28T00:00:00.000+0800", 1300],
+      ["2018-01-29T00:00:00.000+0800", 1400],
+      ["2018-01-30T00:00:00.000+0800", 1500],
+      ["2018-01-31T00:00:00.000+0800", 1600],
+    ];
+    onMounted(() => {
+      if (refDiv.value === undefined) {
+        return;
+      }
+      var myChart = echarts.init(refDiv.value);
+
+
+      myChart.setOption({
+        ...option,
+        series: [
+          {
+            data: data,
+            type: "line",
+            smooth: true,
+            itemStyle: { color: "#ff585d" },
+            areaStyle: {
+              color: {
+                /**
+                 * 线性渐变
+                 * 前四个参数分别是 x0, y0, x2, y2, 范围从 0 - 1，相当于在图形包围盒中的百分比
+                 * 如果 globalCoord 为 `true`，则该四个值是绝对的像素位置
+                 */
+                type: "linear",
+                x: 0,
+                y: 0,
+                x2: 0,
+                y2: 1,
+                colorStops: [
+                  {
+                    offset: 0,
+                    color: "#ff585d", // 0% 处的颜色
+                  },
+                  {
+                    offset: 1,
+                    color: "#ffffff", // 100% 处的颜色
+                  },
+                ],
+                global: false, // 缺省为 false
+              },
+            },
           },
-        },
-        axisLabel:{
-          show:false
-        },
-      },
-      series: [
-        {
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line',
-          smooth: true
-        }
-      ]
-    };
-    myChart.setOption(option);
-  })
-  return () => (<>
-    <div class={s.title}>支出趋势</div>
-    <div ref={refDiv} class={s.wrapper}></div>
-    </>
-  )
-}
-})
+        ],
+      });
+    });
+    return () => (
+      <div class={s.z}>
+        <div class={s.title}>支出趋势</div>
+        <div class={s.lineChartWrapper }>
+          <div ref={refDiv} class={s.wrapper}></div>
+        </div>
+      </div>
+    );
+  },
+});
